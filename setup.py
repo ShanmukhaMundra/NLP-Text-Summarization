cca7cc9b7fcbace33306b2c0682143ac096345b7
shanu0006 <shanumundra66@gmail.com>
2025-05-06 18:41:01 -0400

    requirements & installation setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as f:
+     long_description = f.read()
+
+__version__ = '0.1.0'
+repo_name = 'NLP-Text-Summarization'
+author_user_name = 'ShanmukhaMundra'
+src_repo = 'TextSummarizer'
+author_email = 'shanmukhamundra@yahoo.com'
+
+setuptools.setup(
+    name=src_repo,
+    version=__version__,
+    author=author_user_name,
+    author_email=author_email,
+    description='Python package for NLP',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = f'https://github.com/{author_user_name}/{repo_name}',
+    project_urls={
+        'Bug Tracker': f'https://github.com/{author_user_name}/{repo_name}/issues',
+    },
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src')
+)
+
