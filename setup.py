57b381c8e48167718e5e915d6d3f170f9449e70d
shanu0006 <shanumundra66@gmail.com>
2025-05-06 20:20:20 -0400

    requirements & installation setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding='utf-8') as f:
      long_description = f.read()
 
 __version__ = '0.1.0'
-repo_name = 'NLP-Text-Summarization'
+repo_name = 'NLP-Text-Summarizer'
 author_user_name = 'ShanmukhaMundra'
 src_repo = 'TextSummarizer'
 author_email = 'shanmukhamundra@yahoo.com'
