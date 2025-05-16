e85b32dd3205a01f7da4c649be39a2ca43886753
shanu0006 <shanumundra66@gmail.com>
2025-05-15 23:52:45 -0400

    requirements & installation setup

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+from src.textSummarizer.components.data_validation import DataValiadtion
 from src.textSummarizer.pipeline.phase1_data_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.phase2_data_validation import DataValidationTrainingPipeline
 from src.textSummarizer.logging import logger
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f"stage {STAGE_NAME} started")
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f"stage {STAGE_NAME} completed")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f"stage {STAGE_NAME} started")
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f"stage {STAGE_NAME} completed")
 except Exception as e:
         logger.exception(e)
